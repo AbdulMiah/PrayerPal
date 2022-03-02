2731774d95d4c91a404aed979436b2449ef2b03b
c2035950 <miaham@cardiff.ac.uk>
2022-03-02 14:24:38 +0000

    Modified handleResponse() method to get the location and date, and set the TextView to those values. Added some comments to PrayerTimeFragment. Modified some dimensions

--- 4-application/app/src/main/java/com/example/mob_dev_portfolio/PrayerTimesFragment.java
+++ 4-application/app/src/main/java/com/example/mob_dev_portfolio/PrayerTimesFragment.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -26,16 +27,23 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 public class PrayerTimesFragment extends Fragment {
 
     private ListView lv;
-    private ArrayAdapter<String> arrayAdapter;
     private PrayerListAdapter prayerListAdapter;
     private String[] prayerNamesList;
     private ArrayList<PrayerModel> prayerModels = new ArrayList<PrayerModel>();
+    private Button locationBtn;
+    private TextView currentDateText;
 
     public PrayerTimesFragment() {
         // Required empty public constructor
@@ -46,7 +54,10 @@ public class PrayerTimesFragment extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_prayer_times, container, false);
 
+        this.locationBtn = (Button) v.findViewById(R.id.prayer_location_btn);
+        this.currentDateText = (TextView) v.findViewById(R.id.current_date_text);
         this.lv = (ListView) v.findViewById(R.id.prayer_times_list_view);
+
 
         onAPIRequest(v);
 
@@ -59,20 +70,10 @@ public class PrayerTimesFragment extends Fragment {
         });
 
         return v;
-//        } else {
-//            prayerNamesList = getResources().getStringArray(R.array.prayer_names);
-//            this.arrayAdapter = new ArrayAdapter<String>(
-//                getActivity(),
-//                android.R.layout.simple_list_item_1,
-//                prayerNamesList
-//            );
-//            this.lv.setAdapter(arrayAdapter);
-//            return v;
-//        }
     }
 
     public void onAPIRequest(View view) {
-        String apiUrl = "https://api.pray.zone/v2/times/today.json?city=cardiff";
+        String apiUrl = "https://api.pray.zone/v2/times/today.json?city=london";
         RequestQueue requestQueue = Volley.newRequestQueue(getContext());
 
         JsonObjectRequest apiRequest = new JsonObjectRequest(
@@ -96,28 +97,59 @@ public class PrayerTimesFragment extends Fragment {
     }
 
     public void handleResponse(JSONObject items) {
+        // Check if ArrayList of PrayerModel is empty, if it is then do this...
         if (prayerModels.size() <= 0) {
+            // Initialise some variables
+            String city = "";
+            String country = "";
+            String currentDate = "";
             prayerNamesList = getResources().getStringArray(R.array.prayer_names);
             int listSize = prayerNamesList.length;
-            try {
-                JSONArray ja = items.getJSONObject("results").getJSONArray("datetime");
 
+            try {
+                // Setting JSON Objects and Arrays
+                JSONObject resultsJson = items.getJSONObject("results");
+                JSONArray datetimeArray = resultsJson.getJSONArray("datetime");
+                JSONObject locationObject = resultsJson.getJSONObject("location");
+
+                // Setting current location button to prayer location from API
+                city = locationObject.getString("city");
+                country = locationObject.getString("country");
+                String prayerLocation = city.concat(", "+country);
+                this.locationBtn.setText(prayerLocation);
+//                Log.i("PRAYER LOCATION",prayerLocation);
+
+                // Looping through datetime array to get prayer times and storing them in an ArrayList of PrayerModel objects
                 for (int i=0; i<listSize; i++) {
-                    JSONObject jo = ja.getJSONObject(0).getJSONObject("times");
-//                Log.i(prayerNamesList[i],jo.getString(prayerNamesList[i]));
-                    prayerModels.add(new PrayerModel(prayerNamesList[i], jo.getString(prayerNamesList[i])));
+                    JSONObject prayerTimes = datetimeArray.getJSONObject(0).getJSONObject("times");
+//                Log.i(prayerNamesList[i],prayerTimes.getString(prayerNamesList[i]));
+                    prayerModels.add(new PrayerModel(prayerNamesList[i], prayerTimes.getString(prayerNamesList[i])));
                 }
+
+                // Get current date from API and format it, then set the current date text to formatted date
+                currentDate = datetimeArray.getJSONObject(0).getJSONObject("date").getString("gregorian");
+                DateTimeFormatter apiFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+                DateTimeFormatter myFormat = DateTimeFormatter.ofPattern("EEEE, d MMMM yyyy");
+                LocalDate formatDate = LocalDate.parse(currentDate, apiFormat);
+                String output = myFormat.format(formatDate);
+                currentDateText.setText(output);
+//                Log.i("CURRENT DATE IS...", output);
+
+            // Catch any JSONExceptions and Log to console
             } catch (JSONException e) {
                 Log.e("ERROR!", e.toString());
             }
-            Log.i("NEW PRAYER MODEL",prayerModels.toString());
+        // If ArrayList of PrayerModel is not empty, then display a Toast
         } else {
             Toast.makeText(getActivity(), "You already have the current prayer times", Toast.LENGTH_SHORT).show();
             Log.i("CURRENT PRAYER MODEL",prayerModels.toString());
         }
 
+        // Populate ListView with the prayer times and names
         prayerListAdapter = new PrayerListAdapter(lv.getContext(), prayerModels);
         this.lv.setAdapter(prayerListAdapter);
+
+
 
     }
 
