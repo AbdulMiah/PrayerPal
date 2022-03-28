54004a9f79ab6f71c3abd192ab2e60c01f12173a
c2035950 <miaham@cardiff.ac.uk>
2022-03-28 14:34:44 +0100

    Added Listview component onto Dua layout. Created Dua class to store Dua Objects

--- 4-application/app/src/main/java/com/example/mob_dev_portfolio/fragments/DuaFragment.java
+++ 4-application/app/src/main/java/com/example/mob_dev_portfolio/fragments/DuaFragment.java
@@ -19,6 +19,7 @@ public class DuaFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dua, container, false);
+        View v = inflater.inflate(R.layout.fragment_dua, container, false);
+        return v;
     }
 }
---
+++ 4-application/app/src/main/java/com/example/mob_dev_portfolio/models/Dua.java
@@ -0,0 +1,58 @@
+package com.example.mob_dev_portfolio.models;
+
+public class Dua {
+
+    private int id;
+    private String title;
+    private String arabic;
+    private String transliteration;
+    private String meaning;
+
+    public Dua(int id, String title, String arabic, String transliteration, String meaning) {
+        this.id = id;
+        this.title = title;
+        this.arabic = arabic;
+        this.transliteration = transliteration;
+        this.meaning = meaning;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArabic() {
+        return arabic;
+    }
+
+    public void setArabic(String arabic) {
+        this.arabic = arabic;
+    }
+
+    public String getTransliteration() {
+        return transliteration;
+    }
+
+    public void setTransliteration(String transliteration) {
+        this.transliteration = transliteration;
+    }
+
+    public String getMeaning() {
+        return meaning;
+    }
+
+    public void setMeaning(String meaning) {
+        this.meaning = meaning;
+    }
+}
