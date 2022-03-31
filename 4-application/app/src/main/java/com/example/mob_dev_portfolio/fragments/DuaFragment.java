567d5480cdd73f01e598acb9ade17a317e1a1531
c2035950 <miaham@cardiff.ac.uk>
2022-03-31 18:53:23 +0100

    Got search for Dua working properly

--- 4-application/app/src/main/java/com/example/mob_dev_portfolio/fragments/DuaFragment.java
+++ 4-application/app/src/main/java/com/example/mob_dev_portfolio/fragments/DuaFragment.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,18 +12,9 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SearchView;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
 import com.example.mob_dev_portfolio.R;
 import com.example.mob_dev_portfolio.adapters.DuaListAdapter;
 import com.example.mob_dev_portfolio.models.Dua;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -52,12 +42,12 @@ public class DuaFragment extends Fragment {
         lv = v.findViewById(R.id.dua_lv);
         sv = v.findViewById(R.id.dua_title_sv);
 
-        getDuas(v);
+        getDuas();
 
         return v;
     }
 
-    public void getDuas(View v) {
+    public void getDuas() {
         // Populate ListView with the dua titles
         duaListAdapter = new DuaListAdapter(lv.getContext(), duaArrayList);
         lv.setAdapter(duaListAdapter);
@@ -66,7 +56,7 @@ public class DuaFragment extends Fragment {
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                changeInternalFragment(new DuaDetailFragment(), R.id.main_frag_container, duaArrayList.get(i));
+                changeInternalFragment(new DuaDetailFragment(), R.id.main_frag_container, (Dua) duaListAdapter.getItem(i));
             }
         });
 
